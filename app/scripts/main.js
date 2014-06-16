7305db8af9f3df96b73b0e15504820f337d8db8d
Logan Wade <loganwade@gmail.com>
2014-06-16 17:06:43 -0600

    small changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,12 +3,18 @@ require.config({
         'jquery': 'bower_components/jquery/jquery',
         'underscore': 'bower_components/underscore-amd/underscore',
         'backbone': 'bower_components/backbone-amd/backbone',
-        'leanModal': 'bower_components/jquery-leanmodal/jquery.leanModal'
+        'leanModal': 'bower_components/jquery-leanmodal/jquery.leanModal',
+        'facebook': '//connect.facebook.net/en_US/all'
     },
     'shim' : {
-        'leanModal':['jquery']
+        'leanModal':['jquery'],
+        'facebook' : {
+            exports: 'FB'
+        }
     }
 });
+
+require(['fb']);
 
 require(['scripts/views/home'], function(HomeView) {
     new HomeView();
